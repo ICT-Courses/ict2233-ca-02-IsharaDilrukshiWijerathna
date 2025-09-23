207a1d8d806a36a0ae65c9c7e71b29f21f5693d7
IsharaDilrukshiWijerathna <ar112763@fhss.sjp.ac.lk>
2025-09-23 22:57:57 +0530

    Create the contact page.

--- my-portfolio/src/pages/Contact.jsx
+++ my-portfolio/src/pages/Contact.jsx
@@ -1,8 +1,84 @@
+// import styles
+import "../styles/contact.scss";
+
+//imoport image
+import contactImg from "../assets/images/contact.png";
+
+// import emailjs
+import emailjs from "@emailjs/browser";
+
+//import useRf
+import {useRef} from "react";
+
+//Contact page content
 export default function Contact() {
-  return (
-    <div className="contact-page">
-      <h1>Contact Me</h1>
-      <p>This will contain a contact form later.</p>
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_eek61w5",    // Service ID
+        "template_wxf5e95",   // Template ID
+        form.current,
+        "QbBym5lKgY1crq9jL"   //Public key
+      )
+
+      .then(
+        (result) => {
+          alert ("Message sent successfully!");
+          form.current.reset();
+        },
+        
+        (error) => {
+          alert ("Failed to send message. Try again!")
+        }
+        
+      );
+  };
+
+  return(
+    <div className="contact-page d-flex align-items-center">
+      <div className="container">
+        <div className="row align-items-center">
+
+          {/* left side */}
+          <div className="col-md-6 text-center left-section">
+            <h1 className="contact-title"> Contact</h1>
+            <img
+              src={contactImg}
+              alt="Contact"
+              className="contact-img"
+            />
+          </div>
+
+          {/* right side */}
+          <div className="col-md-6 right-section">
+            <form ref={form} onSubmit={sendEmail}>
+
+              <div className="mb-3">
+                <label className="form-label"> Name</label>
+                <input type="text" name="user_name" className="form-control" required></input>
+              </div>
+
+              <div className="mb-3">
+                <label className="form-label"> Email</label>
+                <input type="email" name="user_email" className="form-control" required></input>
+              </div>
+
+              <div className="mb-3">
+                <label className="form-label"> Message </label>
+                <textarea name="message" className="form-control" rows="4" required></textarea>
+              </div>
+
+              <button type="submit" className="btn btn-primary">Send</button>
+
+            </form>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
