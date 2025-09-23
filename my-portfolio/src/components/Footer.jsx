ceec88df4f6e20746acce66600ca4332777fd98b
IsharaDilrukshiWijerathna <ar112763@fhss.sjp.ac.lk>
2025-09-23 23:43:59 +0530

    Create the footer.

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,10 +1,36 @@
+import githubIcon from "../assets/images/github-icon.png";
+import linkedinIcon from "../assets/images/linkdin.jpg";
+
 export default function Footer() {
   return (
     <footer className="footer bg-dark text-white py-3 mt-5">
-      <div className="container text-center">
-        <p>© {new Date().getFullYear()} Ishara | ICT Undergraduate</p>
+      <div className="container d-flex justify-content-between align-items-center">
+
+        {/* Left side */}
+        <p className="mb-0">© {new Date().getFullYear()} Ishara | ICT Undergraduate</p>
+
+        {/* Right side */}
+        <div className="footer-links">
+          <a
+            href="https://github.com/IsharaDilrukshiWijerathna"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={githubIcon} alt="GitHub" className="footer-icon" />
+          </a>
+
+          <a
+            href="https://linkedin.com/in/ishara-dilrukshi-6111222317"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedinIcon} alt="LinkedIn" className="footer-icon" />
+          </a>
+        </div>
       </div>
     </footer>
   );
 }
+
+
 
